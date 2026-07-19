62bc02b79c700c84b6152f5a988b564c3eccc706
Xymnoz <xelzeroth@gmail.com>
2026-07-19 09:54:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const intro = document.getElementById("intro");
+const main = document.getElementById("main");
+const button = document.getElementById("enterBtn");
+const music = document.getElementById("bgMusic");
 
+button.onclick = () => {
+
+    intro.style.display = "none";
+
+    main.style.display = "flex";
+
+    music.play();
+
+};
